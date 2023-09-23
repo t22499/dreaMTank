20e254cdebbbf6a73e1a2af7e175b8bc14e9d971
t22499 <t2249935621@163.com>
2023-09-23 21:43:48 +0800

    footer动态样式

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import Header from '@/components/Header/Header.vue'
 import Footer from '@/components/Footer/Footer.vue'
-
 </script>
 
 <template>
--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,8 +1,18 @@
 <script setup>
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+};
 </script>
 
 <template>
   <footer class="footer">
+    <a class="to-top" @click="scrollToTop()">
+      <img class="icon-up-arrow" src="../../assets/img/download.png">
+    </a>
     <div class="footer-content">
       <div class="left">
         <a class="left-logo" href="/">
@@ -93,70 +103,197 @@
 </template>
 
 <style lang="scss" scoped>
-.footer{
-  width: 100%;
-  background: #111;
-  overflow: hidden;
-  position: relative;
-  color: #fff;
-  .footer-content{
-    max-width: 1000px;
-    margin: 0 auto;
-    padding: 50px 0 30px;
-    display: flex;
-    // justify-content: space-around;
-    .left{
-      .left-logo{
-        display: flex;
-        align-items: center;
-        font-size: 24px;
-        .logo-icon{
-          margin-right: 6px;
-          border-radius: 100%;
-        }
-        .logo-txt{
-          font-size: 16px;
-          font-weight: 700;
-          color: rgb(255, 255, 255);
-        }
-      }
-      .info{
-        font-size: 14px;
-        line-height: 1.6;
+@media all and (min-width: 1080px){
+  .footer{
+    width: 100%;
+    background: #111;
+    overflow: hidden;
+    position: relative;
+    color: #fff;
+    .to-top{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 70px;
+      height: 70px;
+      background-color: #1b1b1b;
+      text-align: center;
+      color: #fff;
+      font-size: 24px;
+      text-decoration: none;
+      line-height: 70px;
+      transition: all .5s ease;
+      opacity: .6;
+      position: absolute;
+      bottom: 60px;
+      right: 0;
+      z-index: 100;
+      .icon-up-arrow{
+        width: 25px;
       }
     }
-    .right{
-      font-weight: 700;
-      margin-left: 80px;
-      margin-top: 6px;
-      flex: 1 1 0%;
-      .items-wrapper{
-        display: flex;
-        justify-content: space-around;
-       .item{
-        font-size: 100%;
-        font-weight: 400;
-        vertical-align: baseline;
-        margin: 0px;
-        padding: 0px;
-        border-width: 0px;
-        border-style: initial;
-        border-color: initial;
-        border-image: initial;
-        .item-title{
-          color: rgb(255, 255, 255);
-          display: inline-block;
-          margin-bottom: 15px;
-          transition: all 0.5s ease 0s;
-        }
-        .item-child-wrapper{
-          .item-child{
+    .footer-content{
+      max-width: 1000px;
+      margin: 0 auto;
+      padding: 50px 0 30px;
+      display: flex;
+      // justify-content: space-around;
+      .left{
+        .left-logo{
+          display: flex;
+          align-items: center;
+          font-size: 24px;
+          .logo-icon{
+            margin-right: 6px;
+            border-radius: 100%;
+          }
+          .logo-txt{
+            font-size: 16px;
+            font-weight: 700;
             color: rgb(255, 255, 255);
-            display: inline-block;
-            margin-top: 12px;
           }
         }
-       } 
+        .info{
+          font-size: 14px;
+          line-height: 1.6;
+        }
+      }
+      .right{
+        font-weight: 700;
+        margin-left: 80px;
+        margin-top: 6px;
+        flex: 1 1 0%;
+        .items-wrapper{
+          display: flex;
+          justify-content: space-around;
+        .item{
+          font-size: 100%;
+          font-weight: 400;
+          vertical-align: baseline;
+          margin: 0px;
+          padding: 0px;
+          border-width: 0px;
+          border-style: initial;
+          border-color: initial;
+          border-image: initial;
+          .item-title{
+            color: rgb(255, 255, 255);
+            display: inline-block;
+            margin-bottom: 15px;
+            transition: all 0.5s ease 0s;
+          }
+          .item-child-wrapper{
+            .item-child{
+              color: rgb(255, 255, 255);
+              display: inline-block;
+              margin-top: 12px;
+            }
+          }
+        } 
+        }
+      }
+    }
+  }
+}
+
+@media all and (min-width: 0px) and (max-width: 1079px){
+  .footer{
+    width: 100%;
+    background: #111;
+    overflow: hidden;
+    position: relative;
+    color: #fff;
+    .to-top{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 70px;
+      height: 70px;
+      background-color: #1b1b1b;
+      text-align: center;
+      color: #fff;
+      font-size: 24px;
+      text-decoration: none;
+      line-height: 70px;
+      transition: all .5s ease;
+      opacity: .6;
+      position: absolute;
+      bottom: 60px;
+      right: 0;
+      z-index: 100;
+      .icon-up-arrow{
+        width: 25px;
+      }
+    }
+    .footer-content{
+      max-width: 1000px;
+      margin: 0 auto;
+      padding: 50px 0 0;
+      display: flex;
+      flex-direction: column;
+      .left{
+        width: 100%;
+        margin-bottom: 30px;
+        .left-logo{
+          display: flex;
+          align-items: center;
+          justify-items: center;
+          font-size: 24px;
+          position: relative;
+          height: 25px;
+          .logo-icon{
+            margin-right: 6px;
+            border-radius: 100%;
+            position: absolute;
+            top: -10;
+            left: 35%;
+          }
+          .logo-txt{
+            font-size: 16px;
+            font-weight: 700;
+            color: rgb(255, 255, 255);
+            position: absolute;
+            top: 0;
+            left: 40%;
+          }
+        }
+        .info{
+          font-size: 14px;
+          line-height: 1.6;
+          text-align: center;
+        }
+      }
+      .right{
+        width: 100%;
+        flex: 1;
+        .items-wrapper{
+          display: flex;
+          justify-content: space-around;
+          .item{
+            display: none;
+          }
+          .item:nth-child(1){
+            display: block;
+            flex: 1;
+            font-weight: 700;
+            margin-top: 6px;
+            .link-active{
+              width: 100%;
+              background-color: #202020;
+              margin-bottom: 1px;
+              height: 44px;
+              line-height: 44px;
+              transition: all .5s ease;
+              color: #fff;
+              display: inline-block;
+              margin-bottom: 1px;
+              text-align: center;
+              &:hover{
+                opacity: .5;
+              }
+            }
+          }
+        }
       }
     }
   }
